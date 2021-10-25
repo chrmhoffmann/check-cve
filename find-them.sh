b3c98d65d80369a7e381c7ec434d0c27fa463452
Christian Hoffmann <chrmhoffmann@gmail.com>
2021-10-25 23:04:20 +0200

    better

--- find-them.sh
+++ find-them.sh
@@ -23,18 +23,20 @@ function checkout_or_update() {
 function find_cr_in_branches() {
     repo=$1
     asb_file=$2
-
-    cd cve-checker/$repo
+    asb_out_file=$3
     
+    cd cve-checker/$repo
+
+    # cleanup old stuff
     rm -Rf  *
     git checkout -f
-    git checkout $branch 2>&1
 
+    # check branches
     for branch in `git branch -a --sort=-committerdate`; do
 	git checkout $branch
 	while IFS='' read -r QCFIXID || [ -n "${QCFIXID}" ]; do
 	    git log | grep -B 20 "${QCFIXID}" >> $asb_file.out
-	done < $asb_file
+	done < $asb_out_file
     done
     cd -
 }
@@ -50,10 +52,9 @@ function download_asb() {
 }
 
 asb=$1
-
 asb_file=asb_${asb}_crid.txt
+asb_out_file=$asb_file.out
 
-rm -f $asb_file.out
 touch $asb_file
 asb_file=`realpath ${asb_file}`
 
@@ -61,5 +62,5 @@ download_asb $asb $asb_file
 
 for repo in $repos; do
     checkout_or_update $repo
-    find_cr_in_branches $repo $asb_file
+    find_cr_in_branches $repo $asb_file $asb_out_file
 done
